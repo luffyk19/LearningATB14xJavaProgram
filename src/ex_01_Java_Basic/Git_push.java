b63695283e0e28486a806ade06daabdd4e8c9925
luffyk19 <luffyk1995@gmail.com>
2025-09-08 22:02:24 +0530

    Update Git_push.java

--- src/ex_01_Java_Basic/Git_push.java
+++ src/ex_01_Java_Basic/Git_push.java
@@ -3,5 +3,6 @@ package ex_01_Java_Basic;
 public class Git_push {
     public static void main(String[] args) {
         System.out.println("testing the push with IDE");
+        System.out.println("testfrom git")
     }
 }
