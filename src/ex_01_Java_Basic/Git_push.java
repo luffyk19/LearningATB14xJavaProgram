dc345843d1d63461c91ac51c2bc320b6465165ed
luffyk19 <luffyk1995@gmail.com>
2025-09-08 22:07:47 +0530

    Update Git_push.java

--- src/ex_01_Java_Basic/Git_push.java
+++ src/ex_01_Java_Basic/Git_push.java
@@ -4,5 +4,6 @@ public class Git_push {
     public static void main(String[] args) {
         System.out.println("testing the push with IDE");
         System.out.println("testfrom git");
+        System.out.println("yehbuddy");
     }
 }
