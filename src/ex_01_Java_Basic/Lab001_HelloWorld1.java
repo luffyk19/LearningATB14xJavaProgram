ec70db84bbcea2adfd3a910937514e56b207a1ab
luffyk19 <luffyk1995@gmail.com>
2025-09-08 21:44:13 +0530

    test

--- src/ex_01_Java_Basic/Lab001_HelloWorld1.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorld1.java
@@ -1,7 +1,9 @@
 package ex_01_Java_Basic;
 
-public class Lab001_HelloWorld {
+public class Lab001_HelloWorld1 {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("heloooooooo saaar");
+        System.out.println("yess");
     }
+
 }
