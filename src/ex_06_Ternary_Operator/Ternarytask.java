f0ed6ec3a95464f748c46b5863308760a6905700
luffyk19 <luffyk1995@gmail.com>
2025-09-20 23:52:54 +0530

    Update Ternarytask.java

--- src/ex_06_Ternary_Operator/Ternarytask.java
+++ src/ex_06_Ternary_Operator/Ternarytask.java
@@ -2,7 +2,7 @@ package ex_06_Ternary_Operator;
 
 public class Ternarytask {
     public static void main(String[] args) {
-        int number = 3;
+        int number = 4;
         String result = (number % 2 == 0) ? "even" : "odd" ;
         System.out.println(result);
     }
