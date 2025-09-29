23f62ca813cf33779d50dc7081525125f98c9575
luffyk19 <luffyk1995@gmail.com>
2025-09-29 14:20:34 +0530

    new

---
+++ src/ex_08_If_condition/Lab079_if.java
@@ -0,0 +1,19 @@
+package ex_08_If_condition;
+
+public class Lab079_if {
+    public static void main(String[] args)
+    {
+        String User_input = args[0];
+        System.out.println(User_input);
+        int age = Integer.parseInt(User_input);
+        System.out.println(age);
+
+
+
+
+
+
+
+    }
+
+}
---
+++ src/ex_08_If_condition/Lab082_if_Else_scanner.java
@@ -0,0 +1,12 @@
+package ex_08_If_condition;
+
+import java.util.Scanner;
+
+public class Lab082_if_Else_scanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter your age");
+        int age = scanner.nextInt();
+        System.out.println(age);
+    }
+}
