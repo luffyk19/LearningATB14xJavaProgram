5bde335f97c43bfe19ab080071166e5b43ef15c9
luffyk19 <luffyk1995@gmail.com>
2025-10-04 19:26:17 +0530

    new

---
+++ src/ex_10_For_Loop/Lab101_forloop_with_if.java
@@ -0,0 +1,14 @@
+package ex_10_For_Loop;
+
+public class Lab101_forloop_with_if {
+    public static void main(String[] args) {
+        for (int i = 0; i < 18; i++) {
+            if (i > 15){
+                System.out.println("gift");
+            }else {
+                System.out.println("no gift");
+            }
+
+        }
+    }
+}
---
+++ src/ex_10_For_Loop/Lab_99_For_loop.java
@@ -0,0 +1,10 @@
+package ex_10_For_Loop;
+
+public class Lab_99_For_loop {
+    public static void main(String[] args) {
+        for (int kiran = 0; kiran < 10 ; kiran++) {
+            System.out.println(kiran);
+
+        }
+    }
+}
