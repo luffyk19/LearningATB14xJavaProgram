1adc80454dc2c7aeeb7d4e7d6788aa07f5af2437
luffyk19 <luffyk1995@gmail.com>
2025-10-10 21:44:29 +0530

    new

---
+++ src/ex_15_StringBuffer_Builder_StringFunction/Lab146_SB.java
@@ -0,0 +1,10 @@
+package ex_15_StringBuffer_Builder_StringFunction;
+
+public class Lab146_SB {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder("Kiran");
+        stringBuilder.append("koujalgi");
+        stringBuilder.append("69");
+        System.out.println(stringBuilder);
+    }
+}
---
+++ src/ex_15_StringBuffer_Builder_StringFunction/Lab147_Sbuilder.java
@@ -0,0 +1,10 @@
+package ex_15_StringBuffer_Builder_StringFunction;
+
+public class Lab147_Sbuilder {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Kiran");
+        sb.append("koujalgi");
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
