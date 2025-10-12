85341b684c4fbb9d4980bd2abe40a8f10347a555
luffyk19 <luffyk1995@gmail.com>
2025-10-12 12:47:06 +0530

    new

---
+++ src/Task/Challenge3Comparison_Logical_Operators.java
@@ -0,0 +1,20 @@
+package Task;
+
+public class Challenge3Comparison_Logical_Operators {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 5;
+        System.out.println("a == b: " +(a == b));
+        System.out.println("a > b: " +(a>b));
+        System.out.println("a < b: " +(a < b));
+        System.out.println("(a > b) && (a > 0): " + ((a > b) && (a > 0)));
+        System.out.println("(a < b) || (a > 0): "+ ((a < b) || (a > 0)));
+        System.out.println("!(a > b): " + (!(a > b)));
+
+
+
+
+
+    }
+
+}
