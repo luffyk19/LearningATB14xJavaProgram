20eb997246373dccf763fccf17fb2e4483d68595
luffyk19 <luffyk1995@gmail.com>
2025-10-12 12:04:17 +0530

    new

---
+++ src/Task/Challenge2_Arithmetic_Assignment_Operators.java
@@ -0,0 +1,13 @@
+package Task;
+
+public class Challenge2_Arithmetic_Assignment_Operators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println("Addition: " + (a+b));
+        System.out.println("Substraction: " + (a-b));
+        System.out.println("Multiplication: " + (a*b));
+        System.out.println("Division: " + (a/b));
+        System.out.println("Modulus: " +(a%b));
+    }
+}
